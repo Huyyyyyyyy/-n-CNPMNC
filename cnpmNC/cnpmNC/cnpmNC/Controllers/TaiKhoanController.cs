16372609c8628c432f063f3c49ffbba86afe828a
Gia Huy <huy.94.2016@gmail.com>
2023-04-07 13:33:39 +0700

    update TaiKhoan(7/4/2023)

---
+++ cnpmNC/cnpmNC/cnpmNC/Controllers/TaiKhoanController.cs
@@ -0,0 +1,72 @@
+﻿using cnpmNC.Models;
+using cnpmNC.Models.mapTaiKhoan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cnpmNC.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        cnpmNCEntities db = new cnpmNCEntities();
+        // GET: TaiKhoan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DanhSachTaiKhoan()
+        {
+            cnpmNCEntities db = new cnpmNCEntities();
+            var dstk = db.TaiKhoans.ToList();
+            return View(dstk);
+        }
+        public ActionResult ThemTaiKhoan()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemTaiKhoan(TaiKhoan model)
+        {
+            mapTaiKhoan map = new mapTaiKhoan();
+            
+            if (map.ThemMoiTK(model) == true)
+            {
+                return RedirectToAction("DanhSachTaiKhoan");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+        public ActionResult CapNhatTaiKhoan(String TenTK)
+        {
+            var map = new mapTaiKhoan();
+            var TKEdit = map.lichcb(TenTK);
+            return View(TKEdit);
+        }
+
+        [HttpPost]
+        public ActionResult CapNhatTaiKhoan(TaiKhoan model)
+        {
+            mapTaiKhoan map = new mapTaiKhoan();
+            if (map.updateTK(model) == true)
+            {
+                return RedirectToAction("DanhSachTaiKhoan", new { TenTK = model.TenTK });
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+        public ActionResult XoaTaiKhoan(String TenTK)
+        {          
+            mapTaiKhoan map = new mapTaiKhoan();
+            map.XoaTK(TenTK);
+
+            return RedirectToAction("DanhSachTaiKhoan");
+        }
+    }
+}
---
+++ cnpmNC/cnpmNC/cnpmNC/Models/mapTaiKhoan/mapTaiKhoan.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cnpmNC.Models.mapTaiKhoan
+{
+    public class mapTaiKhoan
+    {
+        cnpmNCEntities db = new cnpmNCEntities();
+        public TaiKhoan lichcb(String TenTK)
+        {           
+            return db.TaiKhoans.SingleOrDefault(ma => ma.TenTK == TenTK);
+        }
+        public bool ThemMoiTK(TaiKhoan model)
+        {
+            try
+            {               
+                db.TaiKhoans.Add(model);
+                db.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool updateTK(TaiKhoan model)
+        {          
+            var updateModel = db.TaiKhoans.Find(model.TenTK);
+
+            //check null
+            if (updateModel == null)
+            {
+                return false;
+            }
+
+            // cập nhật giá trị
+            updateModel.TenTK = model.TenTK;
+            updateModel.MatKhau = model.MatKhau;
+            updateModel.QuyenTruyCap = model.QuyenTruyCap;
+            updateModel.HoTen = model.HoTen;
+            updateModel.SoDT = model.SoDT;
+            updateModel.CCCD = model.CCCD;
+
+            db.SaveChanges();
+            return true;
+        }
+        public bool XoaTK(String TenTK)
+        {
+            
+            var modelcb = db.TaiKhoans.Find(TenTK);
+            if (modelcb != null)
+            {
+                db.TaiKhoans.Remove(modelcb);
+                db.SaveChangesAsync();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
